dc8781334f9d741a2317c202170391716458fd7f
Zvikomborero Nyambuya <znyambuya@gmail.com>
2023-11-09 14:30:36 +0200

    Updated js file by adding touch event handling for both drawing and erasing on mobile devices whilst retaining mouse functionality on desktops

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const changeGridButton = document.getElementById('increase-grid-button'); // Upd
 const colorPicker = document.getElementById('color-picker');
 const eraserButton = document.getElementById('eraser-button');
 let isDrawing = false;
+let touchStartX, touchStartY; // Variables to track touch start position
 let eraserMode = false;
 let color = '#333';
 
@@ -33,10 +34,6 @@ function addDragColorListeners() {
             if (e.target.classList.contains('grid-square')) {
                 e.target.style.backgroundColor = getColor();
             }
-        } else if (eraserMode && e.button === 2) {
-            if (e.target.classList.contains('grid-square')) {
-                e.target.style.backgroundColor = 'transparent';
-            }
         }
     });
 
@@ -49,8 +46,6 @@ function addDragColorListeners() {
             if (e.target.classList.contains('grid-square')) {
                 if (e.button === 0 && !eraserMode) {
                     e.target.style.backgroundColor = getColor();
-                } else if (eraserMode && e.button === 2) {
-                    e.target.style.backgroundColor = 'transparent';
                 }
             }
         }
@@ -59,6 +54,57 @@ function addDragColorListeners() {
     container.addEventListener('mouseover', (e) => {
         if (eraserMode && e.target.classList.contains('grid-square')) {
             e.target.style.backgroundColor = 'transparent';
+        }
+    });
+}
+
+function addTouchColorListeners() {
+    let touchStarted = false; // Track if touch event has started
+
+    container.addEventListener('touchstart', (e) => {
+        e.preventDefault(); // Prevent default touch behavior
+        isDrawing = true;
+        touchStartX = e.touches[0].clientX;
+        touchStartY = e.touches[0].clientY;
+        touchStarted = true;
+
+        if (e.target.classList.contains('grid-square')) {
+            if (eraserMode) {
+                e.target.style.backgroundColor = 'transparent'; // Enable eraser mode for touch
+            } else {
+                e.target.style.backgroundColor = getColor();
+            }
+        }
+    });
+
+    container.addEventListener('touchend', () => {
+        isDrawing = false;
+        touchStarted = false;
+    });
+
+    container.addEventListener('touchmove', (e) => {
+        if (isDrawing && touchStarted) {
+            const touchEndX = e.touches[0].clientX;
+            const touchEndY = e.touches[0].clientY;
+
+            if (e.target.classList.contains('grid-square')) {
+                if (eraserMode) {
+                    e.target.style.backgroundColor = 'transparent'; // Enable eraser mode for touch
+                } else {
+                    e.target.style.backgroundColor = getColor();
+                }
+            }
+
+            // Determine if the user is swiping left/right or up/down
+            const deltaX = touchEndX - touchStartX;
+            const deltaY = touchEndY - touchStartY;
+
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                e.preventDefault(); // Prevent horizontal page scrolling
+            }
+
+            touchStartX = touchEndX;
+            touchStartY = touchEndY;
         }
     });
 }
@@ -99,5 +145,5 @@ resetButton.addEventListener('click', () => {
     createGrid(16); // Always reset to 16x16
 });
 createGrid(16);
-
+addTouchColorListeners(); // Add touch event listeners
 
